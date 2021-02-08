7d8f47cd5065b35155931d1edec60acf7ce4d41c
ericguthrie <ericguthrie12@gmail.com>
2021-02-08 19:25:04 +0000

    adding conditional file

---
+++ conditionals.js
@@ -0,0 +1,24 @@
+const sunny = true;
+const warm = false;
+
+if (sunny) {
+  console.log("Bring ur sunny's matey!");
+}
+
+if (warm) {
+  console.log("Bring ur scarf matey!");
+}
+
+console.log("Now you're ready to go outside!");
+
+const temperature = -100;
+
+if (temperature < 0) {
+  console.log("Wear an astronaut suit matey!");
+} else if (temperature < 15) {
+  console.log("Don't wear ur bikini matey!");
+} else {
+  console.log("Let's go swimmin' matey!");
+}
+
+console.log("Now you're ready to go outside!");
