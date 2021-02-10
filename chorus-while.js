ca44534fc63fb91f5b9a924e65c55d6fa050ed65
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-02-10 10:09:46 -0800

    Add files via upload

---
+++ chorus-while.js
@@ -0,0 +1,24 @@
+const chorus = "Arrrrrrrgh let's find some booty";
+let repeat = 5;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun goes down!");
+
+const chorus = "Let's find treasure!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until there's none left!");
+
+const chorus = "Let's boogy!";
+for (let repeat = 0; repeat < 100; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun burns out!");
