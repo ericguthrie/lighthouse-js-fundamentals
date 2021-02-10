2f731aafa18bb5c223d644765a7a8802ac6cda5b
ericguthrie <ericguthrie12@gmail.com>
2021-02-10 18:00:17 +0000

    while loop examples

---
+++ chorus-while.js
@@ -0,0 +1,18 @@
+const chorus = "Arrrrrrrgh let's find some booty";
+let repeat = 5;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun goes down!");
+
+const chorus = "Let's find treasure!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until there's none left!");
