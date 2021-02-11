b8e1d848b4a8e64e29c90df1959d111ec1b3c4b8
ericguthrie <ericguthrie12@gmail.com>
2021-02-11 22:26:08 +0000

    functions practice

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(1090343094574);
+const elevenIsEven = isEven(1391);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
