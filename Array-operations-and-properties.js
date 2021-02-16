9c3494159c1145b59115afd2e1d6431bb92e4081
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-02-15 16:42:30 -0800

    Add files via upload

---
+++ Array-operations-and-properties.js
@@ -0,0 +1,11 @@
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+films[2] = "Jurassic Park";
+films;
+films.push("A.I.");
+films;
+//examples of manipulating arrays//
+director + " is the director of " + films.length + " films";
+films.push("E.T.");
+director + " is the director of " + films.length + " films";
