d503f9b708dbc8eedf3da8a82288c872722aef40
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-02-16 23:01:12 -0800

    Add files via upload

---
+++ objects-driving-mayor-daisy.js
@@ -0,0 +1,33 @@
+function carPassing(cars, speed) {
+  let updatedCars = cars;
+
+  updatedCars.push( {
+    time: Date.now(),
+    speed: speed
+  }
+  );
+
+  return updatedCars;
+}
+
+  return updatedCars;
+}
+
+    const cars = [
+      {
+      time: 1568329654807,
+      speed: 40
+      },
+      {
+      time: 1568329821632,
+      speed: 42
+      },
+      {
+      time: 1568331115463,
+      speed: 35
+      }
+      ]
+      
+      const speed = 38
+      
+      carPassing(cars, speed)
